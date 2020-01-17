0d5c523eac77216094224e6b7dc533008a4c4c86
Yasas1989 <kdyndevinda@gmail.com>
2020-01-17 10:29:11 +0530

    Yasas naveen
    Add product php

---
+++ addproduct.php
@@ -0,0 +1,106 @@
+<?php
+// Start the session
+session_start();
+?>
+<html>
+
+<head>
+
+    <script src="https://www.gstatic.com/firebasejs/7.6.1/firebase-app.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.6.1/firebase-firestore.js"></script>
+    <script src="https://www.gstatic.com/firebasejs/7.6.1/firebase-storage.js"></script>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>Add Product - LKBay.lk</title>
+    <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Montserrat:400,400i,700,700i,600,600i">
+    <link rel="stylesheet" href="assets/fonts/simple-line-icons.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.css">
+    <link rel="stylesheet" href="assets/css/smoothproducts.css">
+</head>
+
+<body>
+    <nav class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar">
+        <div class="container"><a class="navbar-brand logo" href="#">LKBay.lk</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+            <div class="collapse navbar-collapse" id="navcol-1">
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="index.php">Home</a></li>
+                    
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="catalog.php">All Products</a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="cart.php"><?php
+                    
+                    @$pre = $_SESSION["CART"];
+                    @$pre = explode(";",$pre);
+                    @$pre = count($pre)-1;
+                    
+                    echo "My Cart ($pre)";
+                    ?>
+                </a></li>
+                    <li class="nav-item" role="presentation"><a class="nav-link" href="account.php">Account</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main class="page registration-page">
+        <section class="clean-block clean-form dark">
+            <div class="container">
+                <div class="block-heading">
+                    <h2 class="text-info" id="headings">Add a Product</h2>
+                    <?php
+                        $E = $_SESSION["E"];
+                        echo <<<HD
+                                <h4 hidden id="eml" >$E</h3>
+                            HD;
+                    ?>
+                </div>
+
+                <form onsubmit="return false" id="Mforum">
+                    <div class="form-group">
+                        <label for="name">Name</label>
+                        <input class="form-control item" type="text" id="name" required="">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="price">Price (in USD)</label>
+                        <input class="form-control item" type="number" id="price" required="">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="details">Description</label>
+                        <input class="form-control item" type="desc" id="desc" required="">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Company</label>
+                        <input class="form-control item" type="text" id="comp" inputmode="text" required="">
+                    </div>
+
+                    <div class="form-group">
+                        <label for="email">Product Image (400px x 400px is Recommended)</label>
+                        <input class="form-control item" accept="image/*" type="file" id="file" required="">
+                    </div>
+
+                    <button class="btn btn-primary btn-block" id="addbtn">Add</button>
+
+                </form>
+
+            </div>
+        </section>
+    </main>
+    
+    <footer class="page-footer dark">
+        <div class="footer-copyright">
+            <p>© 2020 LKBay ltd.</p>
+        </div>
+    </footer>
+
+    <script src="./addproduct.js"></script>
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/baguettebox.js/1.10.0/baguetteBox.min.js"></script>
+    <script src="assets/js/smoothproducts.min.js"></script>
+    <script src="assets/js/theme.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/zxcvbn/4.2.0/zxcvbn.js"></script>
+</body>
+
+</html>
